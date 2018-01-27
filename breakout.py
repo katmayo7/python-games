462cfbab9c729172ab1c7cd6044e4e113baf806c
katmayo7 <katherine.y.mayo@gmail.com>
2018-01-26 23:15:06 -0500

    bricks
    added bricks to design

--- breakout.py
+++ breakout.py
@@ -14,15 +14,29 @@ fps = pygame.time.Clock()
 screen.fill((255,255,255))
 
 #paddle
-paddle_rect = pygame.Rect(140, 490, 100, 10)
+paddle_rect = pygame.Rect(150, 490, 75, 10)
 paddle = pygame.draw.rect(screen, (139,0,0), paddle_rect)
 
 #ball
-ball_coord = [190, 482]
-ball = pygame.draw.circle(screen, (0,0,0), ball_coord, 8)
-pygame.display.update()
+ball_coord = [190, 484]
+ball = pygame.draw.circle(screen, (0,0,0), ball_coord, 7)
+
+#bricks
+bricks = []
+for i in range(20, 110, 20):
+  for j in range(0, 400, 50):
+    bricks.append((j, i))
+  
+print bricks
+
+colors = [(244, 69, 66), (235, 244, 66), (66, 244, 75), (66, 244, 22), (176, 66, 244)]
+
+for b in bricks:
+  c = (b[1]/20)-1
+  pygame.draw.rect(screen, colors[c], pygame.Rect(b, (100, 20)))
 
 direction = ''
+pygame.display.update()
 
 #main game loop
 while True:
@@ -40,31 +54,39 @@ while True:
       else:
         pygame.event.post(pygame.event.Event(pygame.QUIT))
         
-    #update position
-    #(left, top, width, height)
+  #update position of paddle
+  x = '' 
   if direction == 'RIGHT':
     if paddle_rect.right + 10 <= 400:
       paddle_rect = paddle_rect.move(10, 0)
     else:
-      pass
-      #bounce it off and go left now?
+      paddle_rect = paddle_rect.move(-10, 0)
+      x = 'LEFT'
+      
   if direction == 'LEFT':
-    if paddle_rect.left -10 < 0:
+    if paddle_rect.left - 10 >= 0:
       paddle_rect = paddle_rect.move(-10, 0)
     else:
-      pass
-      #bounce it off and go right now?
-  #direction = ''
+      paddle_rect = paddle_rect.move(10, 0)
+      x = 'RIGHT'
+
+  if x == 'RIGHT':
+    direction = x
+  if x == 'LEFT':
+    direction = x
     
-    #paddle_coord = [140, 490, 100, 10]
+  #ball graphics
   
   screen.fill((255,255,255))
   paddle = pygame.draw.rect(screen, (139, 0, 0), paddle_rect)
-  ball = pygame.draw.circle(screen, (0,0,0), ball_coord, 8)
+  ball = pygame.draw.circle(screen, (0,0,0), ball_coord, 7)
+  for b in bricks:
+    c = (b[1]/20)-1
+    pygame.draw.rect(screen, colors[c], pygame.Rect(b, (100, 20)))
   
   pygame.display.update()
   
   
   fps.tick(20)
 
-#pygame.display.quit()
+pygame.display.quit()
